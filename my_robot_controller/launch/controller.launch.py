0ae8d8383bb68e7806a980f9243f8b6ad30324db
nrilsy <ulusoynuri3@gmail.com>
2024-11-11 11:32:28 +0300

    diffdriveController is added robot movement bugs are fixed - lidar should be added - maybe IMU needs to be added - lidar and inertial-lidar odometry should be tried

--- my_robot_controller/launch/controller.launch.py
+++ my_robot_controller/launch/controller.launch.py
@@ -1,9 +1,24 @@
 from launch import LaunchDescription
+from ament_index_python import get_package_share_path, get_package_share_directory
 from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+import os
 
 
 
 def generate_launch_description():
+
+    # Define the path to your configuration file
+    config_file_path = os.path.join(
+        get_package_share_directory('my_robot_controller'),
+        'config',
+        'my_robot_controllers.yaml'
+    )
+
+
+    use_sim_time = LaunchConfiguration('use_sim_time', default='true')
+
 
     joint_state_broadcaster_spawner = Node(
         package="controller_manager",
@@ -12,22 +27,31 @@ def generate_launch_description():
             "joint_state_broadcaster",
             "--controller-manager",
             "/controller_manager"
-        ]
+        ],
+        output="screen"
     )
+
     
-    simple_controller = Node(
-        package="controller_manager",
-        executable="spawner",
+
+    robot_controller = Node(
+        package='controller_manager',
+        executable='spawner',
         arguments=[
-            "simple_velocity_controller",
+            "my_robot_controller",
             "--controller-manager",
             "/controller_manager"
-        ]
+        ],  # Controller name from YAML config
+        output='screen',
     )
 
 
 
     return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use simulation (Gazebo) clock if true'
+        ),
         joint_state_broadcaster_spawner,
-        simple_controller
+        robot_controller
     ])
